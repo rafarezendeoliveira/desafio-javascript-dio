edb498e4dcd14ae8500e095f92362663dad5c1a2
Rafaela Rezende de Oliveira <148277327+rafarezendeoliveira@users.noreply.github.com>
2023-10-29 18:14:56 -0300

    Update index.js
    Código ajustado para classificação do nível do herói.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log ("Hello, World")
+let NomeHeroi = "Batman"
+let NivelHeroi = 1000
+
+if (NivelHeroi <= 1000) {NivelHeroi = "Ferro";} 
+else if (NivelHeroi >= 1001 && NivelHeroi <= 2000) {NivelHeroi = "Bronze";} 
+else if (NivelHeroi >= 2001 && NivelHeroi <= 5000) {NivelHeroi = "Prata";} 
+else if (NivelHeroi >= 5001 && NivelHeroi <= 7000) {NivelHeroi = "Ouro";} 
+else if (NivelHeroi >= 7001 && NivelHeroi <= 8000) {NivelHeroi = "Platina";} 
+else if (NivelHeroi >= 8001 && NivelHeroi <= 9000) {NivelHeroi = "Ascendente";}
+else if (NivelHeroi >= 9001 && NivelHeroi <= 10000) {NivelHeroi = "Imortal";} 
+else {NivelHeroi = "Radiante";}
+
+console.log(NomeHeroi + " está no nível de " + NivelHeroi);
